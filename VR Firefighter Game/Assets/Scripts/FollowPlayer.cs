7aa8542d8d399b4e92fb8ad64270add57e916459
VinLeclercq <qlqqlc@outlook.fr>
2022-11-10 22:36:56 +0100

    Rémi nous suit maintenant

--- VR Firefighter Game/Assets/Scripts/FollowPlayer.cs
+++ VR Firefighter Game/Assets/Scripts/FollowPlayer.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject follower;
+    public bool behindyou = false;
 
     private GameObject[] player;
+
+    private GameObject root;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectsWithTag("MainCamera");
+        root = follower.transform.root.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         fixerJoueur();
+        if (behindyou)
+        {
+            suivreJoueur();
+        }
     }
 
     Vector3 getPlayerDirection()
@@ -34,6 +43,19 @@ public class FollowPlayer : MonoBehaviour
         }
     }
 
+    float getDistance()
+    {
+        Vector3 _player_pose = player[0].transform.position;
+        Vector3 _follower_pose =  follower.transform.position;
+        float _distance = Vector3.Distance(_follower_pose, _player_pose);
+        if (_distance < 0)
+        {
+            _distance = -_distance;
+        }
+
+        return _distance;
+    }
+
     void fixerJoueur()
     {
         Vector3 _direction = getPlayerDirection();
@@ -41,7 +63,19 @@ public class FollowPlayer : MonoBehaviour
         {
             follower.transform.rotation = Quaternion.LookRotation(_direction);
             follower.transform.Rotate(0,90,0);
+        }
+    }
 
+    void suivreJoueur()
+    {
+        Vector3 _direction = getPlayerDirection();
+        if (_direction != null)
+        {
+            float _distance = getDistance();
+            if (_distance > 2)
+            { 
+                root.transform.Translate(-_direction * Time.deltaTime);
+            }
         }
     }
 }
